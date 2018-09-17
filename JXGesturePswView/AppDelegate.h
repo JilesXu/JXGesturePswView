bb4e7a018172070b13964a0e9f2c47f27187ab6e
Jiles <632036801@qq.com>
2018-09-17 09:41:43 +0800

    Initial Commit

---
+++ JXGesturePswView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXGesturePswView
+//
+//  Created by 徐沈俊杰 on 2018/9/17.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
